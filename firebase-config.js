47cc7dffbeabee22e27295c66675ba84ad942eb4
Moch Aang Ardiansyah XD. <163013943+AangQwerty@users.noreply.github.com>
2025-01-09 22:43:04 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration (pastikan Anda memasukkan nilai yang benar dari Firebase Console)
 const firebaseConfig = {
-  apiKey: "AIzaSyA2xzDudJSG8oJq6Gux_dKJjqfND3RtOzg",
-  authDomain: "animeku-35a3b.firebaseapp.com",
-  projectId: "animeku-35a3b",
-  storageBucket: "animeku-35a3b.firebasestorage.app",
-  messagingSenderId: "845156431605",
-  appId: "1:845156431605:web:ded791fcf2e00ba0c177bc",
-  measurementId: "G-5XKME7KFNQ"
+    apiKey: "AIzaSyA2xzDudJSG8oJq6Gux_dKJjqfND3RtOzg",
+    authDomain: "animeku-35a3b.firebaseapp.com",
+    databaseURL: "https://animeku-35a3b.firebaseio.com",
+    projectId: "animeku-35a3b",
+    storageBucket: "animeku-35a3b.appspot.com",
+    messagingSenderId: "845156431605",
+    appId: "1:845156431605:web:ded791fcf2e00ba0c177bc",
+    measurementId: "G-5XKME7KFNQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
