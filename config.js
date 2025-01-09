92671dc466431026ee0f2d28ca25bdcb65a1163d
Moch Aang Ardiansyah XD. <163013943+AangQwerty@users.noreply.github.com>
2025-01-09 22:42:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
-/*
-    FIREBASE config / database
-    to edit !
-*/
 const firebaseConfig = {
-    apiKey: null,
-    authDomain: null,
-    databaseURL: null,
-    projectId: null,
-    storageBucket: null,
-    messagingSenderId: null,
-    appId: null,
-    measurementId: null
+    apiKey: "AIzaSyA2xzDudJSG8oJq6Gux_dKJjqfND3RtOzg", // Gantilah dengan API Key yang benar
+    authDomain: "animeku-35a3b.firebaseapp.com", // Gantilah dengan authDomain yang benar
+    databaseURL: "https://animeku-35a3b.firebaseio.com", // Gantilah jika menggunakan Realtime Database
+    projectId: "animeku-35a3b",
+    storageBucket: "animeku-35a3b.appspot.com", // Gantilah dengan storageBucket yang benar
+    messagingSenderId: "845156431605",
+    appId: "1:845156431605:web:ded791fcf2e00ba0c177bc",
+    measurementId: "G-5XKME7KFNQ" // Gantilah dengan measurementId yang benar (jika menggunakan analitik)
 };
